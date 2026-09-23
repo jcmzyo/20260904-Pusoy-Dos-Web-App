9564d92471576c6ecd4cde1dca91a69747664218
Jc M <jeromecedrick.magtibay@gmail.com>
2026-09-23 12:51:22 +0800

    P1M4 - Major Patch 2

--- src/orchestrator/GameRunner.ts
+++ src/orchestrator/GameRunner.ts
@@ -46,7 +46,11 @@ export interface RoundResultCheckpoint {
 function yieldToMacrotask(): Promise<void> {
   return new Promise((resolve) => {
     const channel = new MessageChannel();
-    channel.port1.onmessage = () => resolve();
+    channel.port1.onmessage = () => {
+      channel.port1.close();
+      channel.port2.close();
+      resolve();
+    };
     channel.port2.postMessage(undefined);
   });
 }
